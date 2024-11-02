15feea0319eb3ec81c491cea20cc8166c352a8b6
JatinnVaityy <153219683+JatinnVaityy@users.noreply.github.com>
2024-11-02 12:08:21 +0530

    Delete Desktop/CCN/Real-Time-ChatApp directory

--- Desktop/CCN/Real-Time-ChatApp/Api/Models/User.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const Userschema = new mongoose.Schema({
-    username : {type:String ,unique:true},
-    password : String
-}, {timestamps:true})
-
-const UserModel = mongoose.model("User",Userschema)
-module.exports = UserModel
